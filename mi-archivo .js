38eb6fe5adfe2fd596dd55777cb4e0d45e884508
PamelaArtavia <145409261+PamelaArtavia@users.noreply.github.com>
2023-09-18 19:55:52 -0600

    Update mi-archivo .js

--- mi-archivo .js
+++ mi-archivo .js
@@ -1,3 +1,5 @@
 Alert(“Hola mundo_editado”);
 //Hola mundo
 Console.log ("cambios en el archivo");
+https://github.com/users/PamelaArtavia/projects/1/views/1
+https://gist.github.com/PamelaArtavia/a81d93d0a5a495ad911e9f82d2e391e6
